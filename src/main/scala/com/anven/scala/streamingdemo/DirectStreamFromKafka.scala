a27dc69cc33602b34f1995dd1c1fffab9e6a95ed
NingQue001 <296480474@qq.com>
2021-03-07 11:12:30 +0800

    feat：使用KafkaUtils.createDirectStream来创建流 另外，还可以使用KafkaUtils.createRDD来创建RDD

---
+++ src/main/scala/com/anven/scala/streamingdemo/DirectStreamFromKafka.scala
@@ -0,0 +1,39 @@
+package com.anven.scala.streamingdemo
+
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.streaming.kafka010.KafkaUtils
+import org.apache.spark._
+import org.apache.spark.streaming._
+import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
+import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
+
+object DirectStreamFromKafka {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf()
+      .setAppName(s"${this.getClass.getSimpleName}")
+      .setMaster("local")
+    val sc = new StreamingContext(conf, Seconds(1))
+
+    val kafkaParams = Map[String, Object](
+      "bootstrap.servers" -> "node01:9092,node02:9092,node03:9092",
+      "key.deserializer" -> classOf[StringDeserializer],
+      "value.deserializer" -> classOf[StringDeserializer],
+      "group.id" -> "garbage",
+      "auto.offset.reset" -> "latest",
+      "enable.auto.commit" -> (false: java.lang.Boolean)
+    )
+
+    val topics = Array("test")
+
+    val stream = KafkaUtils.createDirectStream(
+      sc,
+      PreferConsistent,
+      Subscribe[String, String](topics, kafkaParams)
+    )
+
+    stream.map(record => {
+      println("kafka: " + record.key())
+      (record.key(), record.value())
+    })
+  }
+}
